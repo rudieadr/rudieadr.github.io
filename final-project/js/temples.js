37f983ab9ae086c038553c76635dc13ef8c1f270
Amanda <rudieadr@gmail.com>
2019-12-20 22:00:34 -0600

    Add files via upload

--- final-project/js/temples.js
+++ final-project/js/temples.js
@@ -1,39 +1,38 @@
-const requestURL = 'https://rudieadr.github.io/final-project/json/temples.json';
+const requestURL = 'json/temples.json';
 fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);
-	
-	const temples = jsonObject['temples'];
-	for (let i=0; i < temples.length; i++) {
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (jsonObject) {
+			console.table(jsonObject);  // temporary checking for valid response and data parsing
+
+			const temple = jsonObject['data'];
+	for (let i=0; i <= temple.length -1; i++) {
 	let card = document.createElement('section');
 	let name = document.createElement('h2');
 	let image = document.createElement('img');
 	let saddress = document.createElement('div');
 	let caddress = document.createElement('div');
-	let phone = document.createElementt('div');
+	let phone = document.createElement('div');
 	let services = document.createElement('div');
 	let ordinaces = document.createElement('div');
 	let closures = document.createElement('div');
 	let history = document.createElement('div');
 	
 	card.setAttribute("id", i);
-	name.textContent=temples[i].name;
-	saddress.textContent=temples[i].contact.saddress;
-	caddress.textContent=temples[i].contact.caddress;
-	phone.textContent=temples[i].contact.phone;
-	services.textContent=temples[i].Services;
-	ordinaces.textContent=temples[i].Ordinance;
-	closures.textContent=temples[i].Closures;
-	history.textContent=temples[i].history;
-	image.setAttribute('src',temples[i].image);
-	image.setAttribute('alt', temples[i].imageAlt);
+	name.textContent= temple.name;
+	saddress.textContent=temple.sAddress;
+	caddress.textContent=temple.cAddress;
+	phone.textContent= temple.phone;
+	services.textContent=temple.services;
+	ordinaces.textContent=temple.ordinance;
+	closures.textContent=temple.closures;
+	history.textContent=temple.history;
+	image.setAttribute('src',temple.image);
+	image.setAttribute('alt', temple.imageAlt);
 	card.appendChild(name);
 	card.appendChild(history);
-	
-	document.querySelector('div.card').appendChild(card);
+	document.getElementById("card").textContent=card;
 	}
 	}
 	);
