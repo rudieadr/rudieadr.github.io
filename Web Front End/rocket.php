dad836d9231cee1da8d71f4365f5661159a6d4a9
Amanda <rudieadr@gmail.com>
2020-02-04 15:42:45 -0600

    Add files via upload

---
+++ Web Front End/rocket.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Amanda Rudie">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <meta name="description" content="home page">
+    <link rel="stylesheet" href="css/large.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lacquer&display=swap" rel="stylesheet">
+    <title>Acme, Inc.</title>
+</head>
+<body id="temp">
+<img src="images/site/underconst.jpeg" alt="under construction" id="under">
+</body>
+</html>
